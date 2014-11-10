0e6d5924011948399d92dc4df3a88bf0b80d7316
Andrew Stine <stine.drew@gmail.com>
2014-11-10 11:23:16 -0500

    changed action name

--- app/controllers/manual_classifier_controller.rb
+++ app/controllers/manual_classifier_controller.rb
@@ -3,7 +3,7 @@ class ManualClassifierController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def sample
+  def show_page
     @domain = Domain.limit(-1).skip(rand(Domain.count)).first
   end
 end
